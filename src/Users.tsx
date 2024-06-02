773f3a7ecdc30d986c0a2cf94685f5466bb96add
Saurabh Pawar <saurabh.pawar@contentstack.com>
2024-06-02 10:43:27 +0530

    testing item sorting

--- src/Users.tsx
+++ src/Users.tsx
@@ -7,23 +7,31 @@ import {
   SortableContext,
   useSortable,
   horizontalListSortingStrategy,
+  rectSortingStrategy,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
 interface User {
   id: number;
   name: string;
+  column: { start: number; end: number };
+  row: { start: number; end: number };
 }
 
 const SortableUser = ({ user }: { user: User }) => {
+  // console.log(user);
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: user.id.toString() });
 
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
-    width: "100px",
+    gridColumn: `${user.column.start} / ${user.column.end}`,
+    gridRow: `${user.row.start} / ${user.row.end}`,
     height: "100px",
+    overflow: "hidden", // Add this line
+    textOverflow: "ellipsis", // Add this line
+    whiteSpace: "nowrap", // Add this line
   };
 
   return (
@@ -43,14 +51,14 @@ const Users = () => {
   const [users, setUsers] = useState<User[]>(data);
   const [inputValue, setInputValue] = useState<string>("");
 
-  const addUser = () => {
-    const newUser: User = {
-      id: Date.now(),
-      name: inputValue,
-    };
-    setInputValue("");
-    setUsers((users) => [...users, newUser]);
-  };
+  // const addUser = () => {
+  //   const newUser: User = {
+  //     id: Date.now(),
+  //     name: inputValue,
+  //   };
+  //   setInputValue("");
+  //   setUsers((users) => [...users, newUser]);
+  // };
 
   const onDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
@@ -64,7 +72,18 @@ const Users = () => {
       const newIndex = users.findIndex(
         (user) => user.id.toString() === over?.id
       );
-      return arrayMove(users, oldIndex, newIndex);
+      // Move item
+      const newUsers = arrayMove(users, oldIndex, newIndex);
+
+      // Update grid positions
+      newUsers.forEach((user, index) => {
+        user.column.start = (index % 6) + 1;
+        user.column.end = user.column.start + 1;
+        user.row.start = Math.floor(index / 6) + 1;
+        user.row.end = user.row.start + 1;
+      });
+
+      return newUsers;
     });
   };
 
@@ -75,6 +94,8 @@ const Users = () => {
           display: "grid",
           gridTemplateColumns: "repeat(6, 1fr)",
           gap: "10px",
+          border: "1px solid black",
+          width: "100%",
         }}
       >
         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
@@ -82,7 +103,7 @@ const Users = () => {
             items={users.map((user) => user.id.toString())}
             strategy={horizontalListSortingStrategy}
           >
-            {users.map((user) => (
+            {users.map((user, index) => (
               <SortableUser key={user.id.toString()} user={user} />
             ))}
           </SortableContext>
--- src/data.js
+++ src/data.js
@@ -2,41 +2,157 @@ export let data = [
   {
     id: 1,
     name: "Samaria",
+    column: {
+      start: 1,
+      end: 2,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 2,
     name: "Gauthier",
+    column: {
+      start: 2,
+      end: 3,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 3,
     name: "Mellisa",
+    column: {
+      start: 3,
+      end: 4,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 4,
     name: "Arabela",
+    column: {
+      start: 4,
+      end: 5,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 5,
     name: "Devon",
+    column: {
+      start: 5,
+      end: 6,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 6,
     name: "Stacee",
+    column: {
+      start: 6,
+      end: 7,
+    },
+    row: {
+      start: 1,
+      end: 2,
+    },
   },
   {
     id: 7,
-    name: "Federica",
+    name: "Federicasdasdads",
+    column: {
+      start: 1,
+      end: 2,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
   },
   {
     id: 8,
     name: "Jecho",
+    column: {
+      start: 2,
+      end: 3,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
   },
   {
     id: 9,
     name: "Alasteir",
+    column: {
+      start: 3,
+      end: 4,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
   },
   {
     id: 10,
     name: "Elston",
+    column: {
+      start: 4,
+      end: 5,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
+  },
+  {
+    id: 11,
+    name: "Jecho11",
+    column: {
+      start: 5,
+      end: 6,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
+  },
+  {
+    id: 12,
+    name: "Alasteir12",
+    column: {
+      start: 6,
+      end: 7,
+    },
+    row: {
+      start: 2,
+      end: 3,
+    },
+  },
+  {
+    id: 13,
+    name: "Elston13",
+    column: {
+      start: 1,
+      end: 2,
+    },
+    row: {
+      start: 3,
+      end: 4,
+    },
   },
 ];
